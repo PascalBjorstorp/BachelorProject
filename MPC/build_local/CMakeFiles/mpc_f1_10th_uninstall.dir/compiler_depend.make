7d24957b631b79b65354aec9acf884888bb21806
akmun23 <akmun23@student.sdu.dk>
2026-03-04 10:45:04 +0100

    Remove obsolete files and scripts related to the mpc_f1_10th package
    - Deleted environment hook scripts and related files that are no longer needed.
    - Removed generated CMake configuration files and package.xml for the mpc_f1_10th package.
    - Cleaned up unnecessary Python scripts used for template generation.
    - Eliminated unused binary files and installation scripts from the build directory.
    - Updated the package index by removing references to the mpc_f1_10th package.

--- MPC/build_local/CMakeFiles/mpc_f1_10th_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for mpc_f1_10th_uninstall.
-# This may be replaced when dependencies are built.
--- MPC/build_local/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jonathan/Documents/GitHub/BachelorProject/MPC
-# Build directory: /home/jonathan/Documents/GitHub/BachelorProject/MPC/build_local
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
