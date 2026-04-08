a8f6ae77d31dc93fe4db627e0bbf9c601edb75b0
f1tenthJetson <pabjo23@student.sdu.dk>
2026-04-08 11:32:58 +0200

    New map

--- MPC/src/riccati_solver.c
+++ MPC/src/riccati_solver.c
@@ -232,7 +232,7 @@ void riccati_solver_pass(
         for (int a = 0; a < nu; a++) {
             float val = 0.0f;
             for (int b = 0; b < nu; b++) {
-                val += Si[a][b] * (r_aug[b] + Bp[b]);
+                val += Si[a][b] * (r_aug_linear[b] + Bp[b]);
             }
             kk[k][a] = -val;
         }
