98c3f18afe24811c56c45b7fbceac95f4dc68e16
f1tenthJetson <pabjo23@student.sdu.dk>
2026-04-09 15:14:43 +0200

    New test ready

--- f1tenth_lateral_planner/config/lateral_planner_config.hpp
+++ f1tenth_lateral_planner/config/lateral_planner_config.hpp
@@ -28,7 +28,7 @@
 
 // Post-overtake merge behavior
 // Multiplies waypoint vx while merging back to baseline (0.9 => 10% slower)
-#define MERGE_BACK_SPEED_FACTOR 0.8
+#define MERGE_BACK_SPEED_FACTOR 1.0
 
 // Published path extent
 #define LOOKAHEAD_POINTS 80
