a1b5b54fbe1adb431c401dd80034ff2c35151e79
Pascal <pabjo23@student.sdu.dk>
2026-04-17 12:48:33 +0200

    Scale parameters for speed and turn

--- f1tenth_system/vesc/vesc_ackermann/include/vesc_ackermann/vesc_to_odom.hpp
+++ f1tenth_system/vesc/vesc_ackermann/include/vesc_ackermann/vesc_to_odom.hpp
@@ -64,6 +64,7 @@ private:
   std::string base_frame_{"ego_racecar/base_link"};
   double speed_to_erpm_gain_{0.0};
   double speed_to_erpm_offset_{0.0};
+  double odom_speed_scale_{1.0};
   double speed_deadband_{0.15};
   double max_dt_sec_{1.0};
 
@@ -73,6 +74,7 @@ private:
   double steering_correction_c2_{0.0};
   double steering_correction_c1_{1.0};
   double steering_correction_c0_{0.0};
+  double steering_model_scale_{1.0};
 
   // Dynamic bicycle model parameters
   double vehicle_mass_{3.314};
